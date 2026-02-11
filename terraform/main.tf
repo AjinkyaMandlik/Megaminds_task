b13d7082a0127156b12b5a34d99e1af2777cae9b
AjinkyaMandlik <145001029+AjinkyaMandlik@users.noreply.github.com>
2026-02-11 19:33:57 +0530

    Rename IAM role to include a suffix

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "data_bucket" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda-execution-role"
+  name = "lambda-execution-role-"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",

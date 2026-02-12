3257eb5e7329902b1da7749fef9352e3ab00a02d
AjinkyaMandlik <145001029+AjinkyaMandlik@users.noreply.github.com>
2026-02-12 11:11:26 +0530

    Refactor AWS resources and add CloudWatch events
    Updated S3 bucket and IAM role names, added CloudWatch event rule and target, and configured Lambda permissions.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,17 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "data_bucket" {
-  bucket = "internship-event-data-v1"
+  bucket = "internship-event-data-ajinkya-v5"
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda-execution-role-v1"
+  name = "lambda-execution-role-ajinkya-v5"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
       Effect = "Allow"
+      Action = "sts:AssumeRole"
       Principal = {
         Service = "lambda.amazonaws.com"
       }
@@ -25,6 +25,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/../lambda"
@@ -32,11 +33,29 @@ data "archive_file" "lambda_zip" {
 }
 
 resource "aws_lambda_function" "data_processor" {
-  function_name = "event-driven-data-processor-ajinkya-2026"
+  function_name = "event-driven-data-processor-ajinkya-2026-v5"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
 
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+resource "aws_cloudwatch_event_rule" "daily_rule" {
+  name                = "daily-data-processing-rule-ajinkya-2026"
+  schedule_expression = "cron(30 6 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule = aws_cloudwatch_event_rule.daily_rule.name
+  arn  = aws_lambda_function.data_processor.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.data_processor.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.daily_rule.arn
 }
